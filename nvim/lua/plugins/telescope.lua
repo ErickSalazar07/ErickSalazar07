783c975cbc91998da1c9bc81fc2d66e2a106ce2f
Erick Salazar <ericksalazar@javeriana.edu.co>
2026-03-27 23:25:26 -0500

    feat: added plugins directory
    added plugins directory for managing various plugins such as colors.lua
    for format and style specifications, harpoon.lua for tracking specific
    files, telescope.lua for fuzzy finding files in medium-large code bases,
    oneliners.lua wich are plugins wich are easy to configure and run
    (oneliners contains fugitive, a git plugin), etc

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,47 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.5",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+    vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        local argc = vim.fn.argc()
+
+        if argc == 0 then
+          builtin.find_files()
+          return
+        end
+
+        if argc == 1 then
+          local arg = vim.fn.argv(0)
+          if vim.fn.isdirectory(arg) == 1 then
+            vim.cmd.cd(arg)
+            builtin.find_files()
+          end
+        end
+      end,
+    })
+  end,
+}
