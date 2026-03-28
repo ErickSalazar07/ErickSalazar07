a0168214475195712a0cf18656380f0e98db1d66
Erick Salazar <ericksalazar@javeriana.edu.co>
2026-03-28 11:31:16 -0500

    feat: added control commands for Git(fugitive)
    added more control and managment commands for plugion fugitive a.k.a git embebed into nvim

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -16,7 +16,9 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
 
 -- Git command with fugitive plugin managment keybindings
 vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Enters Git(fugitive) command for status" })
-vim.keymap.set("n", "<leader>ga", "<cmd>Git commit<CR>", { desc = "Enters Git(fugitive) command for commiting" })
+vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "Enters Git(fugitive) command for commiting" })
+vim.keymap.set("n", "<leader>ga", "<cmd>Git add -p<CR>", { desc = "Enters Git(fugitive) command for adding in patch mode" })
+vim.keymap.set("n", "<leader>gd", "<cmd>Git diff --staged<CR>", { desc = "Enters Git(fugitive) command for diff in the index/stage" })
 vim.keymap.set("n", "<leader>gl", "<cmd>Git log<CR>", { desc = "Enters Git(fugitive) command for checking logs" })
 
 -- ctrl + c system clipboard
